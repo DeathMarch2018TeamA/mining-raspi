65ddf72e871aee1d31335c9d517426dd71584677
Masahiro Nissato <nissato@virgo.is.utsunomiya-u.ac.jp>
2018-05-29 19:18:40 +0900

    add main.cpp, Makefile

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,48 @@
 #include <string>	//string
 #include <stdlib.h>	//rand,srand
 
+#include "comm.hpp"
 #include "calc.h"
+
+using namespace std;
 
 int main()
 {
+  char team_name[] = "shigeCoin";
+  string *p_zero_num, *p_block, *p_nonce;
+  string zero_num, block, nonce;
 
-	//input data
-	std::string zero_size(1,'0');
-	std::string block(10,'a');
+  // seed set for randam_nonce()
+  srand((unsigned) time(NULL));
+  
+  // sign up for server
+  if (!initialize(team_name)) {
+    cout << "failed to initialize socket" << endl;
+    return -1;
+  }
+  p_zero_num = get_zero();
+  zero_num = *p_zero_num;
+  cout << "zero_num:" << p_zero_num << endl;
 
-    //initial value
-	std::string nonce;
+  // nonce calculate
+  for (int i = 0; i < 10; i++) {
+    p_block = get_block();
+    block = *p_block;
+    nonce = calc_nance(zero_num, block);
+    p_nonce = &nonce;
 
-	//set srand
-	srand(2);
+    if (!send_nonce(p_nonce)) {
+      cout << "failed to send nonce" << endl;
+    }
 
-	nonce=calc_nance(zero_size,block);
+    cout << "block:" << *p_block << endl;
+    cout << "nonce:" << *p_nonce << endl;
+  }
 
-	//print nonce
-	std::cout<<"nonce:"<< nonce <<std::endl;
-
-    return 0;
-
+  if (!finalize()) {
+    cout << "failed to finalize socket" << endl;
+  }
+  
+  return 0;
 }
 
